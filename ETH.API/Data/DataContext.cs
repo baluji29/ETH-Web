b82c03a63488c254f1cdd95099f2d020347c22dc
elitetechhub <55743679+elitetechhub@users.noreply.github.com>
2020-06-05 20:44:25 +0530

    InitialCommit

---
+++ ETH.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using ETH.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETH.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
